06e513077cf1af28cd9920b805a3217032133592
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-10-17 15:33:15 -0400

    LICENSE and license headers

--- src/edu/mit/commensalejml/test/kalman/KalmanFilterSimple.java
+++ src/edu/mit/commensalejml/test/kalman/KalmanFilterSimple.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright (c) 2009-2014, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of Efficient Java Matrix Library (EJML).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.mit.commensalejml.test.kalman;
 
 import org.ejml.data.DenseMatrix64F;
 import org.ejml.simple.SimpleMatrix;
 
 /**
- * Based on org.ejml.example.KalmanFilterSimple, which is Apache 2.0 licensed.
- * @author Jeffrey Bosboom <jbosboom@csail.mit.edu>
+ *
+ * @author Peter Abeles, Jeffrey Bosboom <jbosboom@csail.mit.edu>
  * @since 7/8/2014
  */
 public class KalmanFilterSimple implements KalmanFilter {
